9b86cfbc7044f9738b1533e834713aa0c87aa47d
phseiff <31518703+phseiff@users.noreply.github.com>
2020-08-18 21:32:27 +0000

    Deploying to gh-pages from  @ d06bd9cd9cf1387e0e2a2b65360879eec2d0a6e1 🚀

--- build.py
+++ build.py
@@ -25,14 +25,58 @@ with open("index-raw.html", "r") as f:
         )
     content = content.replace("<! the essays content >", essay_content)
 
-with open("index.html", "w+") as f:
-    f.write(content)
+# Parse RSS feed:
+
+
+def extract_item(tag, text):
+    text, text_within_tags = text.split("<" + tag + ">", 1)
+    text_within_tags, text2 = text_within_tags.split("</" + tag + ">", 1)
+    text += text2
+    return text, text_within_tags
+
+
+rss_feed = requests.get(url="https://phseiff.com/phseiff-essays/feed.rss").text
+essay_content = str()
+while "<item>" in rss_feed:
+    rss_feed, rss_item = extract_item("item", rss_feed)
+    _, description = extract_item("description", rss_item)
+    _, title = extract_item("title", rss_item)
+    _, link = extract_item("link", rss_item)
+    _, pubDate = extract_item("pubDate", rss_item)
+    _, image = extract_item("image", rss_item)
+    essay_content += """
+                <div class="col x0.5">
+                    <div class="col-content">
+                        <div class="zoom">
+                            <div class="card">
+                                <div class="card-image">
+                                    <img src="{image}">
+                                    <span class="card-title">{title}</span>
+                                </div>
+                                <div class="card-content">
+                                    {description}
+                                </div>
+                                <div class="card-action">
+                                    <a href="{link}">Read here!</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>""".format(
+                            image=image,
+                            title=title,
+                            description=description,
+                            link=link
+    )
+    print("essay content:", essay_content)
+
+content = content.replace("<! essay cards >", essay_content)
 
 # Mastodon:
 
 new_essays = list()
 for (essay_title, essay_name, essay_content_as_markdown) in new_essays:
-    # ToDo: Find a wa to determine if our essay is new by storing essays in a file after they where first embedded
+    # ToDo: Find a way to determine if our essay is new by storing essays in a file after they where first embedded
     #  into the rss feed. Oh, and before doing that, write the rss-feed-to-html-content-overview-parser!
     mastodon = Mastodon(
         access_token=sys.argv[1],
@@ -49,3 +93,7 @@ for (essay_title, essay_name, essay_content_as_markdown) in new_essays:
 #  * Beide Repos verbinden, so dass pushs in phseiff-essays auch diese Action in phseiff.github.io triggern.
 #  * Online loggen, um log einfacher zu sehen.
 
+# Finally write to index.html:
+
+with open("index.html", "w+") as f:
+    f.write(content)
