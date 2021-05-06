559d2a4db6ecc84fe9144930d62beb3263c574aa
phseiff <31518703+phseiff@users.noreply.github.com>
2021-05-07 01:20:41 +0200

    Let twitter use large preview images for my essays.

--- build.py
+++ build.py
@@ -43,7 +43,7 @@ redirecting_page = """<!DOCTYPE HTML>
         <meta property="og:image" content="{image}"/>
         <meta property="og:url" content="https://phseiff.com/e/{name}"/>
         <meta property="og:description" content="{description}"/>
-        <meta name="twitter:card" content="summary"/>
+        <meta name="twitter:card" content="summary_large_image"/>
         <meta name="twitter:site" content="@phseiff"/>
         <meta name="twitter:creator" content="@phseiff"/>
         <meta name="twitter:image" content="{image}"/>
